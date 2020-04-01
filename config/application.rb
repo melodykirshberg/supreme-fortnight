93ff4c5345f0e5fc030a58f8190db7c76e78659a
Melody <melodykirshberg@gmail.com>
2020-04-01 16:37:23 -0700

    Refactored strong params for portfolio items

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-
+require "action_controller/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,9 +10,6 @@ module DevPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.action_controller.permit_all_parameters = true
   end
 end
