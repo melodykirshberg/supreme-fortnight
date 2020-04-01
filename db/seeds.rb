809b408b7918a9119d1e1f160baddad220aaf96a
Melody <melodykirshberg@gmail.com>
2020-04-01 09:01:27 -0700

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.creat!(
+  title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics Created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -12,7 +20,8 @@
     quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet 
     ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum 
     rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores 
-    alias consequatur aut perferendis doloribus asperiores repellat."
+    alias consequatur aut perferendis doloribus asperiores repellat.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -27,10 +36,10 @@ puts "10 blog posts created"
 
   puts "5 skills created"
 
-  9.times.do |portfolio_item|
+  8.times.do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}" ,
-      subtitle: "My great service",
+      subtitle: "Ruby on Rails",
       body: "The body of the portfolio",
       main_image: "www.placehold.it/600x400",
       thumb_image: "http://placehold.it/350x200"
@@ -38,3 +47,16 @@ puts "10 blog posts created"
 end
 
 puts "9 portfolios created"
+
+  1.times.do |portfolio_item|
+    Portfolio.create!(
+    title: " Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "sdhfjksdhjkfshdkjfhskfhksjdhfkjshdfjkhdsjhfskdj",
+    main_image: "www.placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+puts "1 portfolio created"
+
